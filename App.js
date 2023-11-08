b17838636218e8dcc14c819d5a614085a07bfcd2
unknown <2dam3@HZ353057.H012108.NET>
2023-11-08 10:17:48 +0100

    segunda version

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const App = () => {
 
   const loadData = async () => {
     const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      "https://api.binance.com/api/v3/ticker/24hr"
     );
     const data = await res.json();
     setCoins(data);
@@ -45,8 +45,8 @@ const App = () => {
         style={styles.list}
         data={coins.filter(
           (coin) =>
-            coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
-            coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+            coin.symbol.toLocaleLowerCase().includes("usdt")&&
+            coin.symbol.toLowerCase().includes(search.toLocaleLowerCase())
         )}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => <CoinItem coin={item} />}
